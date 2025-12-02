a08bda2cd1937fdd4d9b08a1d5703964ca77da39
kashishgola <168362735+kashishgola@users.noreply.github.com>
2025-12-02 23:48:50 +0530

    add rightSide pattern file

---
+++ src/array/Documentation.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Documentation {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+    🌟 1. What is an Array? (Beginner Explanation)-
+        An array is a collection of elements (data) stored in continuous memory.
+            ✔ Stores multiple values in a single variable
+            ✔ All elements must be same data type
+            ✔ You can access elements using index (0,1,2...)
+ */
