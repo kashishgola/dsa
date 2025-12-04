a78c2e18c4cb947978bba86ec933b0c902ea1816
kashishgola <168362735+kashishgola@users.noreply.github.com>
2025-12-04 23:15:06 +0530

    add Reverse hill pattern file

---
+++ src/pattern/ReverseHill.java
@@ -0,0 +1,20 @@
+package pattern;
+
+public class ReverseHill {
+    public static void main(String[] args) {
+        System.out.println("Print the program");
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("  ");
+            }
+            for(int k = i; k < n  ; k++){
+                System.out.print("* ");
+            }
+            for(int l = i; l <= n; l++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
