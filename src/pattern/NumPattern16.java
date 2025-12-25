fa735f6c525f7c8abc3099fc194110d37eb073e5
kashishgola <168362735+kashishgola@users.noreply.github.com>
2025-12-25 22:39:21 +0530

    add num pattern16 file

---
+++ src/pattern/NumPattern16.java
@@ -0,0 +1,33 @@
+package pattern;
+
+public class NumPattern16 {
+    public static void main(String[] args) {
+        System.out.println("Print the number pattern");
+        int n = 5, p = 1;
+        for(int i = 1; i < n; i++, p++){
+            for(int j = i; j <= n; j++){
+                System.out.print("  ");
+            }
+            for(int k = 1; k <= i; k++){
+                System.out.print(p+" ");
+            }
+            for(int l = 1; l < i; l++){
+                System.out.print(p+" ");
+            }
+            System.out.println();
+        }
+//=========================================
+        for(int i = 1; i <= n; i++, p++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("  ");
+            }
+            for(int k = i; k <= n; k++){
+                System.out.print(p+" ");
+            }
+            for(int l = i; l < n; l++){
+                System.out.print(p+" ");
+            }
+            System.out.println();
+        }
+    }
+}
