7dd4b77f08cdd63b2f7c6d9f4b07b0b62899cada
kashishgola <168362735+kashishgola@users.noreply.github.com>
2025-12-26 23:09:25 +0530

    add num pattern18 file

---
+++ src/pattern/NumPattern18.java
@@ -0,0 +1,15 @@
+package pattern;
+
+public class NumPattern18 {
+    public static void main(String[] args) {
+        System.out.println("Number Pattern.......");
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            int p = 1;
+            for(int j = 1; j <= i; j++){
+                System.out.print(p++ +" ");
+            }
+            System.out.println();
+        }
+    }
+}
