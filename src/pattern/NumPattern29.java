414cb358ccc6ef7cee4534c604eee74c4e25c25b
kashishgola <168362735+kashishgola@users.noreply.github.com>
2026-01-02 23:03:06 +0530

    add num pattern29 file

---
+++ src/pattern/NumPattern29.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class NumPattern29 {
+    public static void main(String[] args) {
+        System.out.println("print the pattern");
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == n/2+1 || j == n/2+1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
