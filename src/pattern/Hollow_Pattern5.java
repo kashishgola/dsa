00b78453c4ac9c6488f3ef6ff4c5be0fe4211031
kashishgola <168362735+kashishgola@users.noreply.github.com>
2026-01-09 16:10:15 +0530

    add hollow pattern5 file

---
+++ src/pattern/Hollow_Pattern5.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class Hollow_Pattern5 {
+    public static void main(String[] args) {
+        System.out.println("Print the hollow pattern");
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i % 2 != 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
