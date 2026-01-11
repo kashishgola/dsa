bb19e74996a14e388ab182b33c4bfb9f7b64f0ed
kashishgola <168362735+kashishgola@users.noreply.github.com>
2026-01-11 22:17:01 +0530

    add hollow pattern file

---
+++ src/pattern/Hollow_Pattern8.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Hollow_Pattern8 {
+    public static void main(String[] args) {
+        System.out.println("Print the hollow pattern");
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 || i == n || j == 1 || j == n || i == j || i+j == n+1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
